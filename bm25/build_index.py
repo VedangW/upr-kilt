593a3b2ae1e9c44df2ae9c7d54f1f805382a32a9
Vedang Waradpande <vw120@two.cs.rutgers.edu>
2022-11-29 13:40:06 -0500

    Fix java error in indexing

--- bm25/build_index.py
+++ bm25/build_index.py
@@ -2,6 +2,7 @@ import os
 import math
 import pathlib
 import argparse
+import subprocess
 
 from tqdm import tqdm
 
@@ -47,21 +48,31 @@ def build_index(args):
         write_json(outpath, shard)
     write_json(cfg['title_path'], pid2title)
 
+
+    program = ['python', '-m', 'pyserini.index.lucene']
+
     # Create index
     args_external = [
-        '-collection', 'JsonCollection',
-        '-generator', 'DefaultLuceneDocumentGenerator',
-        '-threads', str(args.n_threads),
-        '-input', str(cfg['collection_dir']),
-        '-index', str(cfg['index_dir']),
-        '-storePositions',
-        '-storeRaw'
+        '--collection', 'JsonCollection',
+        '--generator', 'DefaultLuceneDocumentGenerator',
+        '--threads', str(args.n_threads),
+        '--input', str(cfg['collection_dir']),
+        '--index', str(cfg['index_dir']),
+        '--storePositions',
+        '--storeRaw'
     ]
 
-    print(args_external)
+    process = subprocess.Popen(
+        program + args_external, 
+        stdout=subprocess.PIPE
+    )
 
-    JIndexCollection = autoclass('io.anserini.index.IndexCollection')
-    JIndexCollection.main(args_external)
+    output, error = process.communicate()
+    
+    if error:
+        print(f"Error: {error}")
+    
+    print(output.decode("utf-8"))
 
 
 def main():
